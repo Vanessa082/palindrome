59f0dc268632d93bcc94d4b73d0b0760091c2d17
vanessa082 <wahvanessa22@gmail.com>
2023-12-12 17:56:56 +0100

    correcting linter error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inputText = document.querySelector('.txt')
 const button = document.getElementById('btn')
 const output = document.getElementById('output')
 
-function removeWhitespace(str) {
+function removeWhitespace (str) {
   let result = ''
   for (let i = 0; i < str.length; i++) {
     if (str[i] !== ' ') {
@@ -12,7 +12,7 @@ function removeWhitespace(str) {
   return result
 }
 
-function revereString(word) {
+function revereString (word) {
   let reverseWord = ''
   for (let i = word.length - 1; i >= 0; i--) {
     reverseWord += word[i]
